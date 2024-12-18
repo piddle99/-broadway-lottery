6225603448d54d91a3852f41483eedf695868779
piddle99 <48251526+piddle99@users.noreply.github.com>
2024-12-18 09:23:35 -0500

    Update broadway-direct.spec.ts

--- e2e/broadway-direct.spec.ts
+++ e2e/broadway-direct.spec.ts
@@ -12,7 +12,7 @@ const stealth = stealthPlugin();
 chromium.use(stealth);
 
 const urls = [
-  //"https://lottery.broadwaydirect.com/show/aladdin/",
+  "https://lottery.broadwaydirect.com/show/aladdin/",
   "https://lottery.broadwaydirect.com/show/mj-ny/",
   // "https://lottery.broadwaydirect.com/show/six-ny/",
   // "https://lottery.broadwaydirect.com/show/sweeney-todd-ny/",
